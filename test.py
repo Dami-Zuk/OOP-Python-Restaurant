872142317559d9aeb38827408b50dc3cf4526551
Quan Nguyen <157398135+Oplasama@users.noreply.github.com>
2025-04-15 13:04:22 +0300

    create UI for test (unfinished), add salary reduction test in restaurant

---
+++ test.py
@@ -0,0 +1,20 @@
+from menu import *
+from restaurant import Restaurant
+from staff import Staff
+
+def ui():
+    while True:
+        print('Choose an option:')
+        print('1. Create a restaurant')
+        print('2. Create a staff')
+        print('3. Create menu')
+
+        choice = input('type an option: ')
+        if choice == 1:
+            r1 = Restaurant("Q res")
+            r1.
+        if choice == 2:
+            pass
+        if choice == 3:
+            m1 = Menu()
+            
